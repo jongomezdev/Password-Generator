d9b78f8d5756845ca075d822b7e3d3b8a31cfbc9
Jonathan Gomez <jongomezdev@gmail.com>
2020-11-20 12:31:22 -0500

    Generate random letters, numbers and symbols

--- script.js
+++ script.js
@@ -7,8 +7,26 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function randomNum() {
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function randomSymnbol() {
+  let symbols = "!@#$%^&*()?";
+  return symbols[Math.floor(Math.random()) * symbols.length];
+}
+
+console.log(randomLower());
+console.log(randomUpper());
+console.log(randomNum());
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
