eafa056e804f06508b63e99926aff0263e49a1e8
Jonathan Gomez <jongomezdev@gmail.com>
2020-11-20 13:25:46 -0500

    Create user prompts

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +9,34 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+const randomFunc = {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNum,
+  symbol: randomSymbol,
+};
+
+function userPassword() {
+  passLength = prompt("How many characters would you like to use?");
+  if (passLength >= 8 && passLength <= 128) {
+    randomLower = confirm("Would you like to include a lowercase letter?");
+    randomUpper = confirm("Would you like to include an uppercase letter?");
+    randomNum = confirm("Would you like to include a number?");
+    randomSymbol = confirm("Would you like to include a symbol?");
+    if (randomLower || randomUpper || randomNum || randomSymbol) {
+      generatePassword();
+    } else {
+      alert("You must select at least one option to generate a password.");
+    }
+  } else {
+    alert("Password length must have at least 8 characters, no more than 128");
+  }
+}
+
+//Write Functions
 function randomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -19,14 +47,7 @@ function randomUpper() {
 function randomNum() {
   return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-function randomSymnbol() {
-  let symbols = "!@#$%^&*()?";
-  return symbols[Math.floor(Math.random()) * symbols.length];
+function randomSymbol() {
+  const symbols = "!@#$%^&*()?=/";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-console.log(randomLower());
-console.log(randomUpper());
-console.log(randomNum());
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
