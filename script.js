c4cc9551b65a131c07bc0e87953278d267a3b5ab
Jonathan Gomez <jongomezdev@gmail.com>
2020-11-20 14:45:52 -0500

    Test a few changes

--- script.js
+++ script.js
@@ -9,15 +9,10 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-const randomFunc = {
-  lower: randomLower,
-  upper: randomUpper,
-  number: randomNum,
-  symbol: randomSymbol,
-};
+var lower = randomLower;
+var upper = randomUpper;
+var number = randomNum;
+var symbol = randomSymbol;
 
 function userPassword() {
   passLength = prompt("How many characters would you like to use?");
@@ -36,6 +31,25 @@ function userPassword() {
   }
 }
 
+function generatePassword() {
+  let password = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+  if (typesCount === 0) {
+    return "";
+  }
+  // Create Loop
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      password += randomFunc[funcName]();
+    });
+  }
+  console.log(userPassword);
+}
+
 //Write Functions
 function randomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -51,3 +65,10 @@ function randomSymbol() {
   const symbols = "!@#$%^&*()?=/";
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
+function writePassword() {
+  userPassword();
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
