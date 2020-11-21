7495f32fdcd6a13b7cf6fb020bbc4237e3203d45
Jonathan Gomez <jongomezdev@gmail.com>
2020-11-20 21:49:07 -0500

    Finalize the script

--- script.js
+++ script.js
@@ -2,20 +2,44 @@
 const generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  //
   const totalLength = prompt(
     "How many characters should your new password be?"
   );
   if (totalLength >= 8 && totalLength <= 128) {
     alert(`Your new password will be ${totalLength} characters long.`);
-
+    // Prompt user for password criteria
     let upper = confirm("Would you like to include upper case letters?");
     let lower = confirm("Would you like to include lower case letters?");
-    let special = confirm("Would you like to include special characters?");
+    let symbol = confirm("Would you like to include special characters?");
     let number = confirm("Would you like to include numbers?");
+
+    let allChars = "";
+    let password = "";
+    //Validate user input
+    if (upper) {
+      allChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (lower) {
+      allChars += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (symbol) {
+      allChars += "!#$%&()*+,-./:;<=>?@^[\\]^_`{|}~";
+    }
+    if (number) {
+      allChars += "0123456789";
+    }
+    //Generate password that matches the selected criteria
+    for (i = 0; i < totalLength; i++) {
+      password += allChars.charAt(Math.floor(Math.random() * allChars.length));
+    }
+    return password;
+  } else {
+    alert("Your password must be 8-128 characters!!");
   }
 }
 
-// Write password to the #password input
+// Write generated password to the text area
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
